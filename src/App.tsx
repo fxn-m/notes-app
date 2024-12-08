eb25908c5741e005b48ca0aac95e74b9fcc6882b
felix <67316355+fxn-m@users.noreply.github.com>
2024-12-08 13:53:06 +0000

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Counter from '@/components/Counter'
+
+function App() {
+
+  return (
+    <div className='w-screen flex flex-col items-center gap-8'>
+      <h1 className='text-4xl font-bold mt-10'>Hello!</h1>
+      <Counter />
+    </div>
+  )
+}
+
+export default App
